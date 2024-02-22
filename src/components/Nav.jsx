14e1435dfb081c9b17d430313d879254976fa680
T.Pichonneau <theopichonneau@orange.fr>
2024-02-22 15:58:33 +0100

    ajout du router, navigation et des chemins

---
+++ src/components/Nav.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Accueil</Link>
+          </li>
+          <li>
+            <Link to="/realisation">Créations</Link>
+          </li>
+          <li>
+            <Link to="/service">Mes services</Link>
+          </li>
+          <li>
+            <Link to="/blog">Blog</Link>
+          </li>
+          <li>
+            <Link to="/contact">ME contacter</Link>
+          </li>
+          <li>
+            <Link to="/mention-legal">Mentions légal</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
